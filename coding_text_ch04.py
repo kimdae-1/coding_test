ee904e6d3b585fb4e881f343ab3777a4efb00214
kimdae-1 <80327723+kimdae-1@users.noreply.github.com>
2021-08-30 18:04:22 +0900

    Add files via upload

---
+++ coding_text_ch04.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#상하좌우 움직이는 문제
+
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L','R', 'U', 'D']
+
+for plan in plans:
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+        
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+    
+print(x, y)
+
+
+# In[3]:
+
+
+#n+1시 전까지 몇번의 3이 나오는지 세어보기
+n = int(input())
+
+count = 0
+for i in range(n+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+                
+print(count)
+
+
+# In[6]:
+
+
+# 나이트가 움직일 수 있는 경우의 수
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+steps = [(1,2), (1,-2), (-1,2), (-1,-2), (2,1), (2,-1), (-2,1), (-2,-1)]
+result = 0
+
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+    
+    if next_row >= 1 and next_column >= 1 and next_row <= 8 and next_column <= 8:
+        result += 1
+        
+print(result)
+
+
+# In[ ]:
+
+
+n, m = map(int, input().split())
+d = [[0]* m for _ in range(n)]
+
+x, y, direction = map(int, input().split())
+d[x][y] = 1
+
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+    
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+        
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+        
+    else:
+        turn_time += 1
+        
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+    turn_time = 0
+    
+print(count)
+        
+
+
+# In[ ]:
+
+
+
+
